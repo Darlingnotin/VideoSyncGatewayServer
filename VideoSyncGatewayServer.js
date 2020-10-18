b5e4f3358e9b7d441975edac25b24d9452965f4d
Darlingnotin <darlingnotin@hotmail.com>
2020-10-18 13:57:25 -0400

    Adding origin files.

---
+++ VideoSyncGatewayServer.js
@@ -0,0 +1,27 @@
+var fs = require("fs");
+var os = require("os");
+var express = require("express");
+var app = express();
+var port = 7080;
+var server = app.listen(port);
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ server: server });
+
+app.get("/", function (req, res) {
+  fs.createReadStream("videoSync.html").pipe(res);
+});
+app.get('/assets/clickToWatch.png', (req, res) => {
+  res.sendFile('./assets/clickToWatch.png', { root: __dirname });
+});
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(data) {
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
+  });
+});
+console.log("Video Sync Gateway Server");
+console.log("Website URL http://" + os.networkInterfaces().Ethernet[1].address + ":" + port);
